1eb07b620ebe04c2b71dde545a4d2ec3e6daf96a
Derek Belrose <dbelrose@jhu.edu>
2021-04-05 20:07:07 -0400

    release version

--- infrastructure/fossfundApp-k8s.tf
+++ infrastructure/fossfundApp-k8s.tf
@@ -1,26 +1,24 @@
 # This file is used by the exampleApp-k8s application.  The entries in here are for that application only.
 
 locals {
-  app_name   = "fossfundApp-k8s"
+  app_name   = "fossFundApp-k8s"
   namespaces = ["dev", "prod", "test"]
 }
 
-# module "exampleApp-pv" {
-#   source = "git::git@github.com:jhu-library-operations/tf-mod-aws-efs-pv-k8s.git"
-#   #source = "../../tf-mod-aws-efs-pv-k8s"
-#   project_name          = local.app_name
-#   output_path           = format("../manifests/apps/%s/overlays/aws/", local.app_name)
-#   subnet_ids            = module.tf_k8s_cluster.subnet_ids
-#   volume_label          = "webvol"
-#   volume_capacity       = "5Gi"
-#   volume_access_mode    = "ReadWriteMany"
-#   volume_reclaim_policy = "Retain"
-#   tags                  = local.tags
-#   vpc_id                = module.tf_k8s_cluster.vpc_id
-#   vpc_cidr_block        = module.tf_k8s_cluster.cidr_block
-#   namespaces            = local.namespaces
-
-# }
+module "prod-mariadb-pv" {
+  source = "git::git@github.com:jhu-library-operations/tf-mod-aws-efs-pv-k8s.git"
+  project_name          = local.app_name
+  output_path           = format("../manifests/apps/%s/overlays/aws/", local.app_name)
+  subnet_ids            = module.tf_k8s_cluster.subnet_ids
+  volume_label          = "prod-databasevol"
+  volume_capacity       = "5Gi"
+  volume_access_mode    = "ReadWriteMany"
+  volume_reclaim_policy = "Retain"
+  tags                  = local.tags
+  vpc_id                = module.tf_k8s_cluster.vpc_id
+  vpc_cidr_block        = module.tf_k8s_cluster.cidr_block
+  namespaces            = local.namespaces
+}
 
 # module "testpv-dev" {
 #   source = "git::git@github.com:jhu-library-operations/tf-mod-aws-efs-pv-k8s.git"
